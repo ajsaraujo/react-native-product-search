6222801420dbbb281adb1ac95f51fc79fd8423ac
Allan Juan <allanjuansil@gmail.com>
2021-10-16 14:26:43 -0300

    Refactor ProductComponent

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -17,31 +17,41 @@ export default function ProductComponent(props: { product: Product }) {
       paddingBottom: 5,
     },
   });
+
   return (
     <View style={styles.container}>
-      <View style={{ display: "flex", flexDirection: "row" }}>
-        <Text
-          style={{
-            marginRight: 5,
-            lineHeight: 16,
-          }}
-        >
-          {product.name}
-        </Text>
-        {!product.available && (
-          <Text
-            style={{
-              fontSize: 12,
-              lineHeight: 16,
-              color: "#7d7d7d",
-              fontWeight: "500",
-            }}
-          >
-            OUT OF STOCK
-          </Text>
-        )}
-      </View>
+      <ProductName product={product} />
       <Text>{formatCurrency(product.price)}</Text>
+    </View>
+  );
+}
+
+function ProductName(props: { product: Product }) {
+  const { product } = props;
+
+  const styles = StyleSheet.create({
+    container: {
+      display: "flex",
+      flexDirection: "row",
+    },
+    productName: {
+      marginRight: 5,
+      lineHeight: 16,
+    },
+    outOfStockWarning: {
+      fontSize: 12,
+      lineHeight: 16,
+      color: "#7d7d7d",
+      fontWeight: "500",
+    },
+  });
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.productName}>{product.name}</Text>
+      {!product.available && (
+        <Text style={styles.outOfStockWarning}>OUT OF STOCK</Text>
+      )}
     </View>
   );
 }
